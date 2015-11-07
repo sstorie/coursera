3cf7aca8a31b9130e923c50faaafdca0e2ac7c62
Sam Storie <sam.storie@emerson.com>
2015-11-07 09:17:03 -0600

    Completed all exercises for the first assignment.

--- Week 2 - Linear Regression/machine-learning-ex1/ex1/gradientDescentMulti.m
+++ Week 2 - Linear Regression/machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -7,6 +7,11 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
+% temp will be a vector that will hold the next theta values while
+%   we calculate them
+%
+tempTheta = zeros(size(X, 2), 1);
+
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -16,17 +21,16 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
+    
+    % We need to calc a new theta for every feature in the data
+    %   (which is the number of columns in X)
+    %
+    for i = 1:size(X, 2)
+        tempTheta(i) = theta(i) - (alpha / m) * sum(((X * theta) - y) .* X(:,i));
+    end
 
-
-
-
-
-
-
-
-
-
-
+    theta = tempTheta;
+    
     % ============================================================
 
     % Save the cost J in every iteration    
