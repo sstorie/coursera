a1b0f04b058a59591ffa5a6a390f063292f8d01a
Sam Storie <sam.storie@emerson.com>
2015-11-10 06:31:46 -0600

    Completed part 1 of the second assignment.

--- Week 3 - Logistic Regression/ex2/sigmoid.m
+++ Week 3 - Logistic Regression/ex2/sigmoid.m
@@ -3,13 +3,16 @@ function g = sigmoid(z)
 %   J = SIGMOID(z) computes the sigmoid of z.
 
 % You need to return the following variables correctly 
-g = zeros(size(z));
+%g = zeros(size(z));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
-
+% We can use the element-wise operation here to seamlessly calculate
+%   the sigmoid of any scalar, vector or matrix input
+%
+g = 1 ./ (1 + exp(-z))
 
 
 
